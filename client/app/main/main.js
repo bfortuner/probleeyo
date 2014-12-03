1ba10e670be5ad325ab266b62590f7e9f08599fb
bfortuner <bfortuner@gmail.com>
2014-12-03 14:47:05 -0800

    initialized core package with angular-fullstack yo generator

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('probleeAppApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
